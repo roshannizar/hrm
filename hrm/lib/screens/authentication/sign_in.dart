8ac2ddbb248458d299a1411b3eb9d686aceccca8
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-01 21:53:29 +0530

    Create sign_in.dart

---
+++ hrm/lib/screens/authentication/sign_in.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:hrm/services/auth_service.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            FlatButton.icon(
+              onPressed: () async {
+                await AuthService().signInWithMicrosoft();
+              },
+              icon: Icon(Icons.arrow_forward),
+              label: Text('Login with Microsoft'),
+            ),
+             FlatButton.icon(
+              onPressed: () async{
+                await AuthService().signOutMicrosoft();
+              },
+              icon: Icon(Icons.exit_to_app),
+              label: Text('Logout with Microsoft'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
