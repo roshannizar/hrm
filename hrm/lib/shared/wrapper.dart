b6d718d8f30bf275f177cf69ce727621dad2c632
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-01 23:20:58 +0530

    validating wrapper

--- hrm/lib/shared/wrapper.dart
+++ hrm/lib/shared/wrapper.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:hrm/models/user_model.dart';
 import 'package:hrm/screens/authentication/sign_in.dart';
 import 'package:hrm/screens/dashboard/dashboard.dart';
-import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Wrapper extends StatelessWidget {
+class Wrapper extends StatefulWidget {
+  @override
+  _WrapperState createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  String user;
+
+  signInChecker() async{
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    
+    user = prefs.getString('user');
+
+    if(user == null) {
+      Navigator.pushReplacementNamed(context, '/signin');
+    } else {
+      Navigator.pushReplacementNamed(context, '/dashboard');
+    }
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    signInChecker();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<UserModel>(context);
-
-    return user != null ? Dashboard():SignIn();
+    return user != null ? Dashboard() : SignIn();
   }
 }
