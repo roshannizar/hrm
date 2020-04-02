12644dc94dbe54a9e182191b4ffb6bded20f2d1d
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-02 19:49:40 +0530

    ui update in timesheet tile

--- hrm/lib/screens/timesheet/timesheet_tile.dart
+++ hrm/lib/screens/timesheet/timesheet_tile.dart
@@ -11,16 +11,58 @@ class TimesheetTile extends StatefulWidget {
 class _TimesheetTileState extends State<TimesheetTile> {
   @override
   Widget build(BuildContext context) {
+    final String status = widget.timesheetModel.status;
+
     return Container(
-      height: 200,
-      width: 200,
+      width: 280,
       child: Card(
-        color: Colors.deepOrange,
-        child: ListTile(
-          title: Text('${widget.timesheetModel.project}'),
-          subtitle: Text('vola vola'),
-        ),
-      ),
+          color: Colors.blue[300],
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.remove_red_eye, color: Colors.white),
+                ),
+                title: Text('${widget.timesheetModel.project}',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25)),
+                subtitle: Text('${widget.timesheetModel.process}',
+                    style: TextStyle(color: Colors.white)),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 15),
+                alignment: Alignment.centerLeft,
+                child: Text('${widget.timesheetModel.taskType}',
+                    style: TextStyle(color: Colors.white)),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: FlatButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.date_range, color: Colors.white),
+                  label: Text('${widget.timesheetModel.date}',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: FlatButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.panorama_fish_eye,
+                      color: status == 'In Progress'
+                          ? Colors.amber
+                          : status == 'Completed'
+                              ? Colors.green
+                              : Colors.white),
+                  label: Text('${widget.timesheetModel.status}',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
